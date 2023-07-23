9ec6e7022fb26fffdec8c30743cab47259cc6258
ShivaPendela <shivasai65007@gmail.com>
2023-07-23 11:00:17 +0530

    edited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './components/Login';
@@ -42,4 +42,3 @@ function App() {
 }
 
 export default App;
-
